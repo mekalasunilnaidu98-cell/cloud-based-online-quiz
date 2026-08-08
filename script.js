c18eb1047aa522d40e153990ce038882d6df3272
mekalasunilnaidu98-cell <mekalasunilnaidu98@gmail.com>
2026-08-08 11:28:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-
+const questions = [
+    {
+        question: "What does HTML stand for?",
+        options: [
+            "Hyper Text Markup Language",
+            "High Text Machine Language",
+            "Hyperlink Text Markup Language",
+            "Home Tool Markup Language"
+        ],
+        answer: "Hyper Text Markup Language"
+    },
+    {
+        question: "What does CSS stand for?",
+        options: [
+            "Computer Style Sheets",
+            "Cascading Style Sheets",
+            "Creative Style System",
+            "Colorful Style Sheets"
+        ],
+        answer: "Cascading Style Sheets"
+    }
+];
